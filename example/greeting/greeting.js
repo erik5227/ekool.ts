958dffab4951ae2448bf54c1ab6dfc517ca92e3f
erik5227 <puulinn.erik@gmail.com>
2021-04-24 14:30:49 +0300

    Update example

--- example/greeting/greeting.js
+++ example/greeting/greeting.js
@@ -3,14 +3,12 @@ const { EKool } = require('../../lib/index');
 
 // Wrap in async function
 const main = async function(){
-    // Create new EKool instance
-    // It is possible to pass in authentication/refresh token if you have those
-    const ekool = new EKool();
     try {
-        // Login with our credentials.
-        await ekool.login(process.env.EMAIL, process.env.PASSWORD);
+        // Create new EKool instance
+        // It is possible to pass in authentication/refresh token if you have those
+        const ekool = new EKool(await EKool.login(process.env.EMAIL, process.env.PASSWORD));
         // Retreive person data. This is necessary for running other commands
         await ekool.getPersonData();
         console.log(`Hello, ${ekool.personData.name1} ${ekool.personData.name2}!`);
-    } catch (err) {console.error("An error occured")}
+    } catch (err) {console.error(err)}
 }()
