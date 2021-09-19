fb95573a705545157eb3702a76121ad5891bb6db
erik5227 <puulinn.erik@gmail.com>
2021-09-19 19:03:28 +0300

    Correct interfaces and types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
-import { feedItem } from "./interfaces";
+import { eventDetails } from "./interfaces";
 
 export type ekoolDate = string;
 export type gradeType = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 13 | 16 | 17 | 18 | 19 | 20;
-export type feed = feedItem[];
+export type feed = eventDetails[];
